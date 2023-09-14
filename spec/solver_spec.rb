502bf2da3635282fdba87a70fcb638232990e316
Diana Beki <dianabeki54@gmail.com>
2023-09-14 14:47:02 +0300

    Run test

---
+++ spec/solver_spec.rb
@@ -0,0 +1,17 @@
+require_relative '../solver'
+describe Solver do
+  describe '#factorial' do
+    it 'should return error if number is negative' do
+      solve = Solver.new
+      expect { solve.factorial(-2) }.to raise_error(ArgumentError)
+    end
+    it 'should return 1 when number is 0' do
+      solve = Solver.new
+      expect(solve.factorial(0)).to eq(1)
+    end
+    it 'should return 120 if number is 5' do
+      solve = Solver.new
+      expect(solve.factorial(5)).to eq(120)
+    end
+  end
+  
