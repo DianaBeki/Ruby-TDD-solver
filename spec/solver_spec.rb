1811ac1d7827b7e5f1115b10ed9d50d8e254cbb0
Diana Beki <dianabeki54@gmail.com>
2023-09-14 14:48:38 +0300

    Implement TDD

--- spec/solver_spec.rb
+++ spec/solver_spec.rb
@@ -14,4 +14,29 @@ describe Solver do
       expect(solve.factorial(5)).to eq(120)
     end
   end
-  
+  describe '#reverse' do
+    it 'should return olleh if input word is hello' do
+      reversed = Solver.new
+      result = reversed.reverse('hello')
+      expect(result).to eq('olleh')
+    end
+  end
+  describe '#fizzbuzz' do
+    it 'return "fizz" when divisible by 3' do
+      solver = Solver.new
+      expect(solver.fizzbuzz(9)).to eq('fizz')
+    end
+    it 'return "buzz" when divisible by 5' do
+      solver = Solver.new
+      expect(solver.fizzbuzz(10)).to eq('buzz')
+    end
+    it 'return "fizzbuzz" when divisible by 3 and 5' do
+      solver = Solver.new
+      expect(solver.fizzbuzz(15)).to eq('fizzbuzz')
+    end
+    it 'return the number if it is not divisible by 3,5' do
+      solver = Solver.new
+      expect(solver.fizzbuzz(7)).to eq('7')
+    end
+  end
+end
